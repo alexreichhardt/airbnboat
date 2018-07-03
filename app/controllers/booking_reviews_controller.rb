df283544a0fe6b8afc333e8db00e2ff75ba934b3
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-03 17:17:21 +0200

    bookings & bookings-reviews controller and view basic

--- app/controllers/booking_reviews_controller.rb
+++ app/controllers/booking_reviews_controller.rb
@@ -1,16 +1,43 @@
 class BookingReviewsController < ApplicationController
-  def new
-  end
 
-  def create
+  before_action :set_review, only: [ :show]
+
+
+  def index
+    @reviews = BookingReview.all
   end
 
   def show
   end
 
-  def destroy
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @bookingreview = BookingReview.new
   end
 
-  def index
+  def create
+    @review = BookingReview.new(title: params[:booking_review][:title],
+                                content: params[:booking_review][:content],
+                                rating: params[:booking_review][:rating],
+                                user_id: current_user.id,
+                                booking_id: params[:booking_id])
+    @review.save
+
+
+    redirect_to booking_booking_review_path(params[:booking_id], @review.id)
   end
+
+  def destroy
+    @review = BookingReview.find(params[:id])
+    @review.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def set_review
+    @review = BookingReview.find(params[:id])
+  end
+
 end
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,35 @@
 class BookingsController < ApplicationController
-  def index
+
+  before_action :set_booking, only: [:show]
+
+  def index # not needed at first?
+    # @bookings = Booking.all
   end
 
   def show
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(user_id: current_user.id, boat_id: params[:boat_id], start_date: params[:booking][:start_date], end_date: params[:booking][:end_date], license: params[:booking][:license])
+    @booking.save
+    redirect_to boat_booking_path(params[:boat_id], @booking.id)
+
   end
 
-  def destroy
+  def destroy # called from user profile?
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
