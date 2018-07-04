e08605d6ba36c46bcf2bdc07baf8b9e9a0513faf
Alexander Reichhardt <alexander.reichhardt@whu.edu>
2018-07-04 14:27:05 +0200

    redirection and carrying on parameters in view and controller for boats

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,19 +4,17 @@ Boat.delete_all
 User.delete_all
 
 
-user1 = User.create(email: "dam@dln.name", password: "dam@dln.name", password_confirmation: "dam@dln.name")
-user2 = User.create(email: "dam2@dln.name", password: "dam2@dln.name", password_confirmation: "dam2@dln.name")
+user1 = User.create(email: "jakob@lewagon.com", password: "jakob@lewagon.com", password_confirmation: "jakob@lewagon.com")
+user2 = User.create(email: "bora@lewagon.com", password: "bora@lewagon.com", password_confirmation: "bora@lewagon.com")
+user3 = User.create(email: "alex@lewagon.com", password: "alex@lewagon.com", password_confirmation: "alex@lewagon.com")
 
-boat1 = Boat.create(title: "Boat 1", description: "d1", user_id: user1.id, city: "Istanbul", person_capacity: 4, price: 30)
-boat2 = Boat.create(title: "Boat 2", description: "d2", user_id: user1.id, city: "Istanbul", person_capacity: 2, price: 50)
-boat3 = Boat.create(title: "Boat 3", description: "d3", user_id: user1.id, city: "Istanbul", person_capacity: 5, price: 40)
+boat1 = Boat.create(title: "Boat 1", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", user_id: user1.id, city: "Istanbul", person_capacity: 4, price: 30, photo: "image/upload/v1530700826/rasc5gsvrkg34ntnv00q.png")
+boat2 = Boat.create(title: "Boat 2", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", user_id: user1.id, city: "Istanbul", person_capacity: 2, price: 50, photo: "image/upload/v1530700826/rasc5gsvrkg34ntnv00q.png")
+boat3 = Boat.create(title: "Boat 3", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", user_id: user1.id, city: "Istanbul", person_capacity: 5, price: 40, photo: "image/upload/v1530700826/rasc5gsvrkg34ntnv00q.png")
+boat4 = Boat.create(title: "Boat 4", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", user_id: user2.id, city: "Montreal", person_capacity: 5, price: 20, photo: "image/upload/v1530700826/rasc5gsvrkg34ntnv00q.png")
+boat5 = Boat.create(title: "Boat 5", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", user_id: user2.id, city: "Montreal", person_capacity: 5, price: 30, photo: "image/upload/v1530700826/rasc5gsvrkg34ntnv00q.png")
+boat6 = Boat.create(title: "Boat 6", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", user_id: user2.id, city: "Montreal", person_capacity: 6, price: 30, photo: "image/upload/v1530700826/rasc5gsvrkg34ntnv00q.png")
 
-boat4 = Boat.create(title: "Boat 4", description: "d4", user_id: user1.id, city: "Montreal", person_capacity: 5, price: 20)
-boat5 = Boat.create(title: "Boat 5", description: "d5", user_id: user1.id, city: "Montreal", person_capacity: 5, price: 30)
-boat6 = Boat.create(title: "Boat 6", description: "d6", user_id: user1.id, city: "Montreal", person_capacity: 6, price: 30)
-
-puts user1.id
-puts user2.id
 
 # booking = Booking.create(user_id: user2.id, boat_id: boat1.id)
 # review = BookingReview.create(title: "hi", user_id: user2.id, booking_id: booking.id)
