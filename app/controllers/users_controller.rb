9882bdc2ab3e42ac7d00b383c5cdab59f9f2122f
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-06 14:44:40 +0200

    draft

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @boats = Boat.where(user_id: params[:id])
     @bookings = Booking.where(user_id: params[:id])
+
   end
 
   def edit_avatar
