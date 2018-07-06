493c72983f21fd5f0d64d26c189f41ada0928881
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-06 16:50:03 +0200

    seed file with real names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 BookingReview.delete_all
 Booking.delete_all
 Boat.delete_all
@@ -12,7 +14,6 @@ avatars = ["bora.jpg", "jakob.jpg", "alex.jpg"]
   user = User.create(email: "#{users[i]}@lewagon.com", password: "#{users[i]}@lewagon.com", password_confirmation: "#{users[i]}@lewagon.com")
   user.avatar = File.open(Rails.root.join("db", "avatars", avatars[i]))
   user.save
-
 end
 
 
@@ -21,7 +22,7 @@ cities = ["Milano"]
 photos = ["boat1.jpg", "boat2.jpg"]
 
 8.times do |i|
-  boat = Boat.create(title: "Boat #{i}", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur", user_id: User.all.pluck(:id).sample, city: cities.sample, person_capacity: rand(1..10), price: rand(50..1000), start_date: "2018-07-10", end_date: "2018-08-30")
+  boat = Boat.create(title: Faker::FunnyName.two_word_name, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur", user_id: User.all.pluck(:id).sample, city: cities.sample, person_capacity: rand(1..10), price: rand(50..1000), start_date: "2018-07-10", end_date: "2018-08-30")
   boat.photo = File.open(Rails.root.join("db", "seeds", photos.sample))
   boat.save
 end
@@ -47,6 +48,6 @@ Booking.all.each do |booking|
   user_ids << booking.user_id
 end
 15.times do
-  review = BookingReview.create(title: "nice cruise", content: "dummy text ever since the 1500 dummy text ever since the 1500 dummy text ever since the 1500", rating: rand(1..5), user_id:  user_ids.sample, booking_id:  Booking.all.pluck(:id).sample)
+  review = BookingReview.create(title: "nice cruise", content: "dummy text ever since the 1500 dummy text ever since the 1500 dummy text ever since the 1500", rating: rand(3..5), user_id:  user_ids.sample, booking_id:  Booking.all.pluck(:id).sample)
   review.save
 end
